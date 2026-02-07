9a44eb9afa29211d86d4479a816067e26dc5293f
Iván <jukk4p@gmail.com>
2026-02-07 05:05:52 +0000

    REVISA EL PACKAGE.JSON TAMBIEN POR SI HUBIERA ALGUN ERROR

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -11,12 +12,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
